5417e22eb3255a6db972cbfef1c62578b30431e3
Shihab <shihabuddin2469@gmail.com>
2025-08-04 21:55:59 +0600

    buid auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router";
 import { router } from "./Router/Router.jsx";
+import AuthProvider from "./Context/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="urbanist">
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </div>
   </StrictMode>
 );
